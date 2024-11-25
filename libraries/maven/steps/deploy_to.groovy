01c2cf269ea567475da8b23e20bcf65977a5dce4
ssrilakshmanan <ssrilakshmanan@salesforce.com>
2024-11-25 21:01:12 +1100

    Included custom variable APP_NAME

--- libraries/maven/steps/deploy_to.groovy
+++ libraries/maven/steps/deploy_to.groovy
@@ -1,6 +1,6 @@
 void call(app_env){
   // use the default long_name property to dynamically name the stage
   stage("Deploy to ${app_env.long_name }"){
-		echo "Deploying...........   ${app_env.CH_ENV }"
+		echo "Deploying...   ${app_env.APP_NAME }   to  ${app_env.CH_ENV }"
   }
 }

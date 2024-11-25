546e19a8c08493b10d3d373330463508045d76ad
ssrilakshmanan <ssrilakshmanan@salesforce.com>
2024-11-25 20:41:26 +1100

    updated application_environment

--- pipeline_configuration/pipeline_config.groovy
+++ pipeline_configuration/pipeline_config.groovy
@@ -1,19 +1,18 @@
 allow_scm_jenkinsfile = false
 
 libraries {
-    testlib
-	//maven
+	maven
 }
 
-application_environment {
+application_environment{
 	merge = true
 	override = false
-	dev {
+	dev{
 		CH_ENV = "DEV"
 		MULE_ENV = "dev"
 	}
 	
-	qa {
+	qa{
 		CH_ENV = "QA"
 		MULE_ENV = "qa"	
 	}

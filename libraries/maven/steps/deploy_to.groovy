fdcd44eee3da5f146856ee23612ff508d7d1a3ea
ssrilakshmanan <ssrilakshmanan@salesforce.com>
2024-11-25 20:55:54 +1100

    Included custom variable CH_ENV

--- libraries/maven/steps/deploy_to.groovy
+++ libraries/maven/steps/deploy_to.groovy
@@ -1,6 +1,6 @@
 void call(app_env){
   // use the default long_name property to dynamically name the stage
   stage("Deploy to ${app_env.long_name }"){
-		echo "Deploying..........."
+		echo "Deploying...........   ${app_env.CH_ENV }"
   }
 }
--- pipeline_configuration/pipeline_config.groovy
+++ pipeline_configuration/pipeline_config.groovy
@@ -5,6 +5,7 @@ libraries {
 application_environments{
   dev{
     ip_addresses = [ "1.2.3.4", "1.2.3.5" ]
+	CH_ENV = "DEV"
   }
   test
   prod{
